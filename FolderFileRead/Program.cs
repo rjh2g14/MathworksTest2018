19f53d79c0ff279c4b183d075df3b231a613dcd2
Richard <richard.hall@blueyonder.co.uk>
2018-07-24 16:45:17 +0100

    Object Orientated

--- FolderFileRead/Program.cs
+++ FolderFileRead/Program.cs
@@ -5,58 +5,86 @@ using System.IO;
 
 namespace FolderFileRead
 {
+
+    public class WordFrequency
+    {
+        public string FilePath { get; set; }
+
+        Dictionary<string, int> Totalcounts = null;
+
+        public WordFrequency()
+        {
+            Totalcounts = new Dictionary<string, int>();
+        }
+
+        public Dictionary<string, int> processFile(string filename)
+        {
+            Dictionary<string, int> Wordcounts = new Dictionary<string, int>();
+
+            string contents = File.ReadAllText(filename);
+
+            contents = contents.Replace("\n", " ");
+            contents = contents.Replace("\r", " ");
+            contents = contents.Replace("\t", " ");
+
+            char[] seperators = new char[] { ' ', '.', ',' };
+
+            string[] Words = contents.Split(seperators, StringSplitOptions.RemoveEmptyEntries);  //Modifies the contents to be put into an array, seperated by delimiters and removing 'empty' entries
+
+            //Each of the loops below works through the array for each file, adding it to the unique dictionary and the world dictionary
+
+            foreach (var item in Words)
+            {
+                if (Totalcounts.Keys.Contains(item))
+                {
+                    Totalcounts[item]++;
+                }
+                else
+                {
+                    Totalcounts.Add(item, 1);
+                }
+
+            }
+
+            foreach (var item in Words)
+            {
+                if (Wordcounts.Keys.Contains(item))
+                {
+                    Wordcounts[item]++;
+                }
+                else
+                {
+                    Wordcounts.Add(item, 1);
+                }
+
+            }
+
+            return Wordcounts;
+        }
+
+        public Dictionary<string, int> getTotalCounts()
+        {
+            return Totalcounts;
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-
             var directory = @"C:\Users\rhbox\Desktop\Test";    // Set the file directory here. This will also be where the results file is created
-
             StreamWriter sw = new StreamWriter(directory + @"\Results.rtf");
 
             var Totalcounts = new Dictionary<string, int>();  //Dictionary containing words accross all files
 
+            WordFrequency wf = new WordFrequency();
+
             foreach (string file in Directory.EnumerateFiles(directory, "*.txt"))  //Enumerates through all files in the directory and sub-directoraries endining in .txt
             {
-                var Wordcounts = new Dictionary<string, int>(); // A dictionary for each individual file
 
-                string contents = File.ReadAllText(file);
-
-                contents = contents.Replace("\n", " ");
-                contents = contents.Replace("\r", " ");
-                contents = contents.Replace("\t", " ");
-
-                char[] seperators = new char[] { ' ', '.', ',' };
-
-                string[] Words = contents.Split(seperators, StringSplitOptions.RemoveEmptyEntries);  //Modifies the contents to be put into an array, seperated by delimiters and removing 'empty' entries
-
-                //Each of the loops below works through the array for each file, adding it to the unique dictionary and the world dictionary
-                foreach (var item in Words)
-                {
-                    //If the item is in the dictionary, it will only increase its value by one. If not, it will add the word to the dictionary.
-                    if (Wordcounts.Keys.Contains(item))
-                    {
-                        Wordcounts[item]++;
-                    }
-                    else
-                    {
-                        Wordcounts.Add(item, 1);
-                    }
-
-                }
-
-                foreach (var item in Words)
-                {
-                    if (Totalcounts.Keys.Contains(item))
-                    {
-                        Totalcounts[item]++;
-                    }
-                    else
-                    {
-                        Totalcounts.Add(item, 1);
-                    }
-
-                }
+                Dictionary<string, int> Wordcounts = wf.processFile(file);
 
                 Console.WriteLine(' ');
                 Console.WriteLine(file);
@@ -73,7 +101,7 @@ namespace FolderFileRead
 
             }
 
-
+            Totalcounts = wf.getTotalCounts();
 
             //Orders the world dictionary and takes the top ten values
             var toptentotal = (from entry in Totalcounts orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value).Take(10);
